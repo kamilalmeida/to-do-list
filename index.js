f175fdd914e3f45860159cadaafd2edd32ae0e89
Kamila Silva <kamila.silva@pmweb.com.br>
2022-10-07 14:54:05 -0300

    finalizando projeto

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-// const toDoList = [];
+function getTodosLocalStorage() {
+  return JSON.parse(localStorage.getItem("todo")) ?? [];
+}
 
-function createItem(item, index, done = false) {
+function saveLocalStorage(toDoList) {
+  localStorage.setItem("todo", JSON.stringify(toDoList));
+}
+
+function createItem(item, done, index) {
   const elementDiv = document.querySelector(".list");
 
   const elementList = `
-            <li data-index=${index} class="item" >
+            <li class="item ${done ? "done" : ""}">
                 <div class="list" >
-                    <input class="checkbox" type="checkbox" id="checkbox" class="checked" >
+                    <input class="checkbox checked" type="checkbox" id=${index} ${
+    done && "checked"
+  }>
                 </div>
                 <label for="checkbox" >
                 ${item}
                </label>
-                <button class="delete" type="button" >
-                  <svg class="del xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-                  <path d="M294.6 166.6L317.3 144 272 98.7l-22.6 22.6L160 210.7 70.6 121.4 48 98.7 2.7 144l22.6 22.6L114.7 256 25.4 345.4 2.7 368 48 413.3l22.6-22.6L160 301.3l89.4 89.4L272 413.3 317.3 368l-22.6-22.6L205.3 256l89.4-89.4z"/>
+                <button class="delete" type="button">
+                  <svg class="no-select" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                  <path class="no-select" d="M294.6 166.6L317.3 144 272 98.7l-22.6 22.6L160 210.7 70.6 121.4 48 98.7 2.7 144l22.6 22.6L114.7 256 25.4 345.4 2.7 368 48 413.3l22.6-22.6L160 301.3l89.4 89.4L272 413.3 317.3 368l-22.6-22.6L205.3 256l89.4-89.4z"/>
                   </svg>
                 </button>
             </li>
           `;
   elementDiv.innerHTML += elementList;
-  const elementLi = document.querySelector(".item");
-  console.log(done);
-
- 
 }
 
 function updateScreen() {
   const toDoList = getTodosLocalStorage();
+
   document.querySelector(".list").innerHTML = "";
-  toDoList.forEach((item, index) => createItem(item.text, index, item.done));
 
   if (toDoList.length > 0) {
+    toDoList.forEach((item, index) => createItem(item.text, item.done, index));
     document.querySelector(".warning").classList.add("disabled");
   } else {
     document.querySelector(".warning").classList.remove("disabled");
@@ -46,7 +51,7 @@ function insertItem(event) {
   const toDoList = getTodosLocalStorage();
 
   if (dataInput != "") {
-    toDoList.push({ text: dataInput, done: true });
+    toDoList.push({ text: dataInput, done: false });
 
     saveLocalStorage(toDoList);
     updateScreen();
@@ -57,18 +62,13 @@ function insertItem(event) {
 
 document.addEventListener("click", (event) => {
   const targetEl = event.target;
-  const parentEl = targetEl.closest("li");
-  let todoTitle = targetEl.closest("li").innerText;
+  const targetIndex = targetEl.getAttribute("id");
 
   if (targetEl.type === "checkbox") {
-    parentEl.classList.toggle("done");
-    updateTodoStatusLocalStorage(todoTitle);
+    updateTodoStatusLocalStorage(targetIndex);
   }
   if (targetEl.classList.contains("del") || targetEl.classList.contains("delete")) {
-    console.log("oi");
-    parentEl.remove();
-
-    removeTodoLocalStorage(todoTitle);
+    removeTodoLocalStorage(targetIndex);
   }
 });
 
@@ -77,10 +77,8 @@ function getSearchedTodos(search) {
 
   todos.forEach((todo) => {
     const todoTitle = todo.querySelector("label").innerText.toLowerCase();
-    console.log(todoTitle);
     todo.style.display = "flex";
     if (!todoTitle.includes(search)) {
-      console.log("oi");
       todo.style.display = "none";
     }
   });
@@ -95,7 +93,7 @@ searchInput.addEventListener("keyup", (e) => {
 
 function filterTodos(filterValue) {
   const todos = document.querySelectorAll(".item");
-  console.log(todos);
+
   switch (filterValue) {
     case "all":
       todos.forEach((todo) => (todo.style.display = "flex"));
@@ -133,30 +131,20 @@ filterBtn.addEventListener("change", (e) => {
   filterTodos(filterValue);
 });
 
-function getTodosLocalStorage() {
-  return JSON.parse(localStorage.getItem("todo")) ?? [];
-}
-
-function saveLocalStorage(toDoList) {
-  console.log(toDoList);
-  localStorage.setItem("todo", JSON.stringify(toDoList));
-}
-
-function removeTodoLocalStorage(todoTitle) {
-  const todos = getTodosLocalStorage();
-  const filteredTodos = todos.filter(function (todo) {
-    // console.log(todo.text);
-    todo.text != todoTitle;
-    console.log("todoText:", todoTitle, "doArray:", todo.text);
-  });
-  console.log(filteredTodos);
-
-  localStorage.setItem("todo", JSON.stringify(filteredTodos));
-}
-function updateTodoStatusLocalStorage(todoTitle) {
+function removeTodoLocalStorage(index) {
   const todos = getTodosLocalStorage();
 
-  todos.map((todo) => (todo.text === todoTitle ? (todo.done = !todo.done) : null));
+  todos.splice(index, 1);
 
-  localStorage.setItem("todo", JSON.stringify(todos));
+  saveLocalStorage(todos);
+  updateScreen();
+}
+
+function updateTodoStatusLocalStorage(index) {
+  const todos = getTodosLocalStorage();
+
+  todos[index].done = !todos[index].done;
+
+  saveLocalStorage(todos);
+  updateScreen();
 }
